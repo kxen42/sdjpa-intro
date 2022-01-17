5ed86b0adfa40eb17065fb6b276de7c7ff9a9ab0
kxen42 <kxen42@users.noreply.github.com>
2022-01-16 18:49:04 -0600

    Use MySQL Compatibility Mode For H2

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -1,15 +1,18 @@
 package guru.springframework.sdjpaintro;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import org.junit.jupiter.api.*;
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 
-import guru.springframework.sdjpaintro.domain.Book;
-import guru.springframework.sdjpaintro.repositories.BookRepository;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Created by jt on 7/3/21.
@@ -17,6 +20,7 @@ import guru.springframework.sdjpaintro.repositories.BookRepository;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class SpringBootJpaTestSlice {
 
     @Autowired
