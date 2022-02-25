7e07bc1df478e8ab17bc2b2223c38954bb321766
kxen42 <kxen42@users.noreply.github.com>
2022-02-25 10:43:20 -0600

    JPA @Query Examples

--- src/main/java/guru/springframework/sdjpaintro/jpa/repository/BookJpaRepository.java
+++ src/main/java/guru/springframework/sdjpaintro/jpa/repository/BookJpaRepository.java
@@ -1,9 +1,14 @@
 package guru.springframework.sdjpaintro.jpa.repository;
 
 import java.util.Optional;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.Nullable;
+import org.springframework.scheduling.annotation.Async;
 
 import guru.springframework.sdjpaintro.jpa.domain.BookJpa;
 
@@ -15,4 +20,21 @@ public interface BookJpaRepository extends JpaRepository<BookJpa, Long> {
 
   @Nullable
   BookJpa getByTitle(@Nullable String title);
+
+  Stream<BookJpa> findAllByTitleNotNull();
+
+  @Async
+  Future<BookJpa> queryByTitle(String title);
+
+  // ?1 is positional parameter of 1
+  // I was right - the Spring can't find the 'table' in the query, the ApplicationContext will not
+  // load
+  @Query("FROM BookJpa b WHERE b.title=?1")
+  BookJpa findBookByTitleWithAnnotation(String title);
+
+  @Query("FROM BookJpa b WHERE b.title=:title")
+  BookJpa findBookByTitleWithQueryNamedParam(@Param("title") String title);
+
+  @Query(value = "SELECT * FROM book_jpa WHERE title = :title", nativeQuery = true)
+  BookJpa findBookByTitleNativeQuery(@Param("title") String title);
 }
--- src/test/java/guru/springframework/sdjpaintro/jpa/BookJpaRepositoryTest.java
+++ src/test/java/guru/springframework/sdjpaintro/jpa/BookJpaRepositoryTest.java
@@ -1,6 +1,11 @@
 package guru.springframework.sdjpaintro.jpa;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
+import guru.springframework.sdjpaintro.jpa.domain.BookJpa;
 import guru.springframework.sdjpaintro.jpa.repository.BookJpaRepository;
 
 /** Testing Repository directly to demo nullable handling. */
@@ -45,5 +51,48 @@ public class BookJpaRepositoryTest {
         () -> {
           repository.getByTitle("Bogus");
         });
+  }
+
+  @Test
+  @DisplayName("Demo Stream Query")
+  void findAllByTitleNotNull() {
+    // Using AtomicInteger because Functional Programming expects you to use Immutable objects
+    AtomicInteger count = new AtomicInteger();
+    repository
+        .findAllByTitleNotNull()
+        .forEach(
+            book -> {
+              count.incrementAndGet();
+            });
+
+    assertThat(count.get()).isGreaterThanOrEqualTo(5);
+  }
+
+  @Test
+  @DisplayName("@Async Query Returning Future")
+  void queryByTitle() throws ExecutionException, InterruptedException {
+    Future<BookJpa> bookFuture = repository.queryByTitle("Clean Code");
+    BookJpa book = bookFuture.get();
+
+    assertNotNull(book);
+  }
+
+  @Test
+  void findBookByTitleWithAnnotation() {
+    BookJpa found = repository.findBookByTitleWithAnnotation("Clean Code");
+    System.out.println("BookJpaRepositoryTest.findBookByTitleWithAnnotation found:" + found);
+    assertNotNull(found);
+  }
+
+  @Test
+  void findBookByTitleWithQueryNamedParam() {
+    BookJpa found = repository.findBookByTitleWithQueryNamedParam("Clean Code");
+    assertNotNull(found);
+  }
+
+  @Test
+  void findBookByTitleNativeQuery() {
+    BookJpa found = repository.findBookByTitleNativeQuery("Clean Code");
+    assertNotNull(found);
   }
 }
